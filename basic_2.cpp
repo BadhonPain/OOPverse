eee0b2f2097d49072c7e06b1e93e41d3ab8fe25c
Test_Case <badhonpain48@gmail.com>
2025-11-30 02:25:34 +0600

    setter-getter

--- basic_2.cpp
+++ basic_2.cpp
@@ -2,19 +2,32 @@
 using namespace std;
 class Teacher
 {
-    private: 
+private:
     double salary;
-public: //
+
+public:
     string name;
     string dept;
     string subject;
-
+    void setSalary(double s) // setter function
+    {
+        salary = s;
+    }
+    double getSalary() // getter function
+    {
+        return salary;
+    }
 };
 
 int main()
 {
     Teacher t1;
     t1.name = "Badhon";
-    t1.salary = 250000;
+    // t1.salary = 250000; we can't print salary now in this way bcz the salary is private but we can print using stter and getter
+    // double z;
+    // cin >> z;
+    // t1.setSalary(z); user input
+    t1.setSalary(100000);
+    cout << t1.getSalary() << endl;
     cout << t1.name << endl;
 }
