489db82e0f856c5a28b701a1849f16b06163f7af
Test_Case <badhonpain48@gmail.com>
2025-11-30 01:30:11 +0600

    innitial commit

---
+++ basic_syntax.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Teacher {
+    public:
+    string name;
+    string dept;
+    string subject;
+    double salary;
+};
+
+int main()
+{
+    Teacher t1;
+    t1.name = "Badhon";
+    t1.salary = 250000;
+    cout<< t1.name << endl;
+
+
+
+
+}
